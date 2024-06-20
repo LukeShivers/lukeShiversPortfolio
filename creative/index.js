354f4b08b5a80266efaeeaf0a72aa1a021f718dd
lukeshivers <shiverld@mail.uc.edu>
2024-06-20 16:33:54 -0400

    bug fixes

--- creative/index.js
+++ creative/index.js
@@ -174,3 +174,24 @@ document.addEventListener("DOMContentLoaded", function () {
     default:
   }
 });
+
+const preloaderContainer = document.querySelector(".preloaderContainer");
+const planeContainer = document.querySelector(".planeContainer");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    if (window.innerWidth <= 600) {
+      preloaderContainer.style.transform = "translate(400rem, 0)";
+    } else {
+      preloaderContainer.style.transform = "translate(500rem, 0)";
+    }
+  }, 2000);
+});
+
+const preloaderPlane = bodymovin.loadAnimation({
+  container: planeContainer,
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "../preloaderPlane.json",
+});
